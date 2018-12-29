bb3aa846a2e5bcd671bd86ae697cd17358f7d907
Bahadır Tülüce <eminbahadir98@gmail.com>
2018-12-29 23:01:07 +0300

    Fixed foreign keys while populating.

--- database/populate_tables.sql
+++ database/populate_tables.sql
@@ -10,9 +10,9 @@ INSERT INTO Country(name) VALUES("Japan");
 
 INSERT INTO Account(username, email, passwd, first_name, middle_name, last_name)
     VALUES("bahadir", "bahadir@example.com", "123", "Emin", "Bahadir", "Tuluce");
-INSERT INTO CustomerAccount(national_ID, nationality, gender, date_of_birth)
-    VALUES("12345678912", 1, "Male", "1998-02-18");
+INSERT INTO CustomerAccount(ID, national_ID, nationality, gender, date_of_birth)
+    VALUES(LAST_INSERT_ID(), "12345678912", (SELECT ID FROM Country WHERE name="Turkey"), "Male", "1998-02-18");
 
 INSERT INTO Account(username, email, passwd, first_name, middle_name, last_name)
     VALUES("abdullah", "abdullah@example.com", "123", "Abdullah", NULL, "Talayhan");
-INSERT INTO StaffAccount() VALUES();
+INSERT INTO StaffAccount(ID) VALUES(LAST_INSERT_ID());
