98f051746d6322c6b96115089bd37ef0677e2302
Mahmud Sami Aydın <mahmudsami97@gmail.com>
2018-12-30 20:28:13 +0300

    homepages

---
+++ website/staff_homepage.php
@@ -0,0 +1,75 @@
+<?php
+   include('util/session.php');
+   include('util/visuals.php');
+?>
+
+<html>
+   
+   <head>
+      <title>IBITUR - Home</title>
+      <link rel="stylesheet" href="style/style.css"/>
+      <link rel="stylesheet" href="lib/bootstrap.min.css"/>
+   </head>
+   
+   <body class="content">
+
+      <?php
+         if ($logged_in) {
+            echo get_header($current_fullname);
+         } else {
+            header("location: login.php");
+         }
+      ?>
+
+
+      <?php
+         if (isset($_GET['registered']) && $logged_in) {
+            echo
+            "<div class='alert alert-success' role='alert'>
+               You have successfully registered.
+            </div>";
+         }
+      ?>
+
+      <h1 class="home-title">IBITUR - Tour Reservation</h1>
+
+      <div class="home-content">
+        <h2>Your Created Tours</h2>
+	<?php
+	  $tours_query = "SELECT T.ID FROM Tour T, Account A WHERE T.creator_ID = A.ID AND A.username ='".$_SESSION['session_username']."';";
+	  $tours_result = mysqli_query($db,$tours_query);
+	  if($tours_result->num_rows <> 0 )
+	  {
+		while($row = $tours_result->fetch_assoc()){
+	         $tour_query = "SELECT * FROM TourPreview WHERE tour_ID =".$row["ID"].";";
+                 $tour_result = mysqli_query($db,$tour_query);
+                 if( $tour_result->num_rows == 1 )
+                  {
+                    $tour_row = $tour_result->fetch_assoc();
+		?>
+                 <div class="cart">
+                   <img src=<?php echo $tour_row["image_path"]; ?> >
+                   <label> <?php echo $tour_row["name"]; ?></label>
+                   <label> <?php echo $tour_row["start_date"];?></label>
+                   <label> <?php echo $tour_row["end_date"];?></label>
+                   <label> <?php echo $tour_row["description"]; ?></label>
+                   <label> <?php echo $tour_row["price"]."TRY"; ?></label>
+		   <a href=<?php echo "view_tour.php?id=".$tour_row["tour_ID"]; ?>>TODO</a>
+                 </div>
+	<?php
+                  }
+		}
+	  }else{
+		echo "<h3>You Have No Reservation.</h3>";
+	  }
+	?>
+      </div>
+
+      <?php
+         echo get_footer();
+      ?>
+
+   </body>
+   
+</html>
+
